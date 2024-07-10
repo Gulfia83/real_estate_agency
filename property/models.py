667e289250982c46aecca44bb946874fe3320b71
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-07-10 15:39:56 +0300

    Add Owner model

--- property/models.py
+++ property/models.py
@@ -78,3 +78,16 @@ class Claim(models.Model):
 
     def __str__(self):
         return f'От {self.user.username} на {self.flat.address}'
+
+
+class Owner(models.Model):
+    owner = models.CharField('ФИО владельца', max_length=200)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца',
+                                        blank=True)
+    flats = models.ManyToManyField(Flat,
+                                   related_name='owners',
+                                   verbose_name='Квартиры в собственности')
+    
+    def __str__(self):
+        return f'{self.owner} владеет квартирой'
