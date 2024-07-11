7b2562c3737f51b4a427e8577645c1e310bc7601
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-07-11 10:18:30 +0300

    Add relation between Flat and Owner

--- property/models.py
+++ property/models.py
@@ -5,10 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца',
-                                        blank=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField('Является новостройкой',
                                        default=False, blank=True)
     created_at = models.DateTimeField(
@@ -81,13 +77,17 @@ class Claim(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner = models.CharField('ФИО владельца',
+                             max_length=200,
+                             db_index=True)
+    owners_phonenumber = models.CharField('Номер владельца',
+                                          max_length=20)
     owner_pure_phone = PhoneNumberField('Нормализованный номер владельца',
-                                        blank=True)
+                                        blank=True,
+                                        db_index=True)
     flats = models.ManyToManyField(Flat,
                                    related_name='owners',
                                    verbose_name='Квартиры в собственности')
     
     def __str__(self):
-        return f'{self.owner} владеет квартирой'
+        return f'{self.owner}'
