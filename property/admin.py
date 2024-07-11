d11538251953494b9ab00f055bf4759f6dba7f70
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-07-11 10:54:52 +0300

    Add display owners on flats page

--- property/admin.py
+++ property/admin.py
@@ -3,7 +3,19 @@ from django.contrib import admin
 from .models import Flat, Claim, Owner
 
 
+class ClaimAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flat',)
+
+
+class OwnerFlatInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ('owner', 'flat')
+
+
 class FlatAdmin(admin.ModelAdmin):
+    inlines = [
+        OwnerFlatInline
+    ]
     search_fields = ('town', 'address')
     readonly_fields = ['created_at']
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
@@ -12,11 +24,11 @@ class FlatAdmin(admin.ModelAdmin):
     raw_id_fields = ('liked_by',)
 
 
-class ClaimAdmin(admin.ModelAdmin):
-    raw_id_fields = ('flat',)
-
-
 class OwnerAdmin(admin.ModelAdmin):
+    inlines = [
+        OwnerFlatInline
+    ]
+    exclude = ['flats']
     raw_id_fields = ('flats',)
     search_fields = ('owner', 'owner_pure_phone')
 
